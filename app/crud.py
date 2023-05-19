8fa8cf58047791e139a892ba10bd6674117c6338
timkn <timknothe21@gmail.com>
2023-05-20 00:01:51 +0200

    changed email to username

--- app/crud.py
+++ app/crud.py
@@ -16,7 +16,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(email=user.email)
+    db_user = models.User(username=user.username)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
