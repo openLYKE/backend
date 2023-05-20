9d476c51b7628baf94c82fe36aaeab406eb69715
timkn <timknothe21@gmail.com>
2023-05-20 09:49:15 +0200

    fixed: mysql dialect varchar length

--- app/database.py
+++ app/database.py
@@ -11,5 +11,4 @@ SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
