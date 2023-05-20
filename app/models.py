3f4a8dd17272f6eb8320358c351c3a92ae4030d5
allesklardy <adrian@tum.social>
2023-05-20 09:57:26 +0200

    Fix: imports

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-from .database import Base
+from database import Base
 
 
 class User(Base):
