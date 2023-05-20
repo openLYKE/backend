e894b3427ff4f28f1f0cf5e8201a47ebd30a7194
timkn <timknothe21@gmail.com>
2023-05-20 10:08:39 +0200

    added autopep8 - updated requirements.txt - formatted with autopep8

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- app/main.py
+++ app/main.py
@@ -22,7 +22,8 @@ def get_db():
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
+        raise HTTPException(
+            status_code=400, detail="Username already registered")
     return crud.create_user(db=db, user=user)
 
 
